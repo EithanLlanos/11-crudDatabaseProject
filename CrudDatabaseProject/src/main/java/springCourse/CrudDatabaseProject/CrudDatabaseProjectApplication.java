d4318243292977647ba8b5d89335b83896f2103c
Rubick <eithanllanosb@gmail.com>
2024-12-17 12:56:52 -0500

    200~Set up simple sql-script and spring initilizr implementation

---
+++ CrudDatabaseProject/src/main/java/springCourse/CrudDatabaseProject/CrudDatabaseProjectApplication.java
@@ -0,0 +1,13 @@
+package springCourse.CrudDatabaseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDatabaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDatabaseProjectApplication.class, args);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# 11-crudDatabaseProject
+The following dependencies were included using Spring Initializr:
+
+- Spring Boot DevTools
+- MySQL Driver
+- Spring Data JPA
+- Spring Web
+- Spring Security
+- Thymleaf
+- Validation
