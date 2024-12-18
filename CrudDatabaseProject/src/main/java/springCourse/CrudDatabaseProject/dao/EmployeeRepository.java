fa2bea24c84f0abc408edce0c980293de68e42ed
Rubick <eithanllanosb@gmail.com>
2024-12-18 12:44:44 -0500

    Initialize sort method already implemented by JPA query-methods

--- CrudDatabaseProject/src/main/java/springCourse/CrudDatabaseProject/dao/EmployeeRepository.java
+++ CrudDatabaseProject/src/main/java/springCourse/CrudDatabaseProject/dao/EmployeeRepository.java
@@ -4,5 +4,10 @@ package springCourse.CrudDatabaseProject.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import springCourse.CrudDatabaseProject.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // add a method to sort by last name, this method is handled completely by JpaRepository
+    //See https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html for more information
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
